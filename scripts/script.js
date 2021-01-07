cc723f726979e6bdb6fb77177a55462529be7b2b
maxstain <firaschabchoub@hotmail.com>
2021-01-07 02:08:14 +0100

    first commit

---
+++ scripts/script.js
@@ -0,0 +1,76 @@
+var canvas = document.getElementById('myCanvas');
+var ctx = canvas.getContext('2d');
+const canvWidth = canvas.width;
+const canvHeight = canvas.height;
+var x = canvWidth / 2;
+var y = canvHeight / 2;
+var dx = 2;
+var dy = 2;
+
+function start() {
+    create();
+    draw(x, y);
+    setTimeout(() => {
+        start();
+    }, 10);
+}
+
+function clear() {
+    ctx.fillStyle = 'white';
+    ctx.fillRect(0, 0, canvWidth, canvHeight);
+    ctx.strokeRect(0, 0, canvWidth, canvHeight);
+}
+
+function create() {
+    ctx.fillStyle = 'red';
+    ctx.fillRect(canvWidth / 2 - 20, 0, 60, 40);
+    ctx.strokeRect(canvWidth / 2 - 20, 0, 60, 40);
+    ctx.fillStyle = 'green';
+    ctx.fillRect(0, canvHeight / 2 - 20, 40, 60);
+    ctx.strokeRect(0, canvHeight / 2 - 20, 40, 60);
+    ctx.fillStyle = 'brown';
+    ctx.fillRect(canvWidth - 10, canvHeight / 2 - 20, 10, 60);
+    ctx.strokeRect(canvWidth - 10, canvHeight / 2 - 20, 10, 60);
+}
+
+function draw(x, y) {
+    clear();
+    create();
+    ctx.fillStyle = 'blue';
+    ctx.fillRect(x, y, 20, 20);
+    ctx.strokeRect(x, y, 20, 20);
+}
+
+document.addEventListener('keydown', movePlayer);
+
+function movePlayer(e) {
+    var key = e.key;
+    switch (key) {
+        case "ArrowUp": {
+            if (y != 0) {
+                y -= dy;
+            }
+            break;
+        }
+        case "ArrowLeft": {
+            if (x != 0) {
+                x -= dx;
+            }
+            break;
+        }
+        case "ArrowDown": {
+            if (y != canvHeight - 20) {
+                y += dy;
+            }
+            break;
+        }
+        case "ArrowRight": {
+            if (x != canvWidth - 20) {
+                x += dx;
+            }
+            break;
+        }
+    }
+}
+
+start();
