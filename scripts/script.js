0a701f8ba4f8e8b64cb0be8b00dd84eca6b4e038
maxstain <firaschabchoub@hotmail.com>
2021-01-12 19:34:51 +0100

    feat: working on the mechanics

--- scripts/script.js
+++ scripts/script.js
@@ -78,6 +78,16 @@ function movePlayer(e) {
     }
 }
 
-document.addEventListener("click", () => {});
+document.addEventListener("click", (e) => {
+    var mouseX = e.clientX;
+    var mouseY = e.clientY;
+    console.log(canvWidth);
+    console.log(mouseX);
+    for (let i = 131; i < 191; i++) {
+        if ((mouseX == canvWidth - 2) && (mouseY == canvHeight - i)) {
+            alert("Door");
+        }
+    }
+});
 
 start();
